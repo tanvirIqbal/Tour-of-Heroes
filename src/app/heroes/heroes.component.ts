3274391a11e3d41bfe7d67e130c7d57effa9061b
Tanvir <iqbaltanvir@outlook.com>
2017-12-12 17:04:10 +0600

    Http Add Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -36,7 +36,14 @@ export class HeroesComponent implements OnInit {
   //   this.selectedHero = hero;
   // }
 
- 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 
   ngOnInit() {
     //Part 5
