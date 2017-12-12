8ab3f4e7d34dba90910a7858a2335ca63fed987f
Tanvir <iqbaltanvir@outlook.com>
2017-12-12 16:28:12 +0600

    Error Handler

--- src/app/heroes/heroes.service.ts
+++ src/app/heroes/heroes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 // Observable Step 1 : Import
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of'
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -33,9 +34,29 @@ export class HeroesService {
 
   /** GET heroes from the server */
 getHeroes (): Observable<Hero[]> {
-  return this.http.get<Hero[]>(this.heroesUrl)
+  return this.http.get<Hero[]>(this.heroesUrl).pipe(catchError(this.handleError('getHeroes', [])));
+  
 }
 
+/**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
